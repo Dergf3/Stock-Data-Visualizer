a4cf9f6792097b3a74931a5b7f2346d0055abccf
jamesshawhan <70238458+jamesshawhan@users.noreply.github.com>
2022-10-27 10:25:26 -0500

    Update pygal_graph_interpreter.py

--- pygal_graph_interpreter.py
+++ pygal_graph_interpreter.py
@@ -11,19 +11,19 @@ class Stocks:
         self._populate_chart_list()
         self.date_list = []
 
-def __populate_chart_list(self):
-    for key, value in self.stock_data.data_dictionary.items():
-        self.date_list.append(key)
-        for keyTwo, valueTwo in value.items():
-            if 'open' in keyTwo:
-                valueTwo_number = float(valueTwo)
-                self.open_list.append(valueTwo_number)
-            if 'high' in keyTwo:
-                valueTwo_number = float(valueTwo)
-                self.high_list.append(valueTwo_number)
-            if 'low' in keyTwo:
-                valueTwo_number = float(valueTwo)
-                self.low_list.append(valueTwo_number)
-            if 'close' in keyTwo:
-                valueTwo_number = float(valueTwo)
-                self.close_list.append(valueTwo_number)            
+    def __populate_chart_list(self):
+        for key, value in self.stock_data.data_dictionary.items():
+            self.date_list.append(key)
+            for keyTwo, valueTwo in value.items():
+                if 'open' in keyTwo:
+                    valueTwo_number = float(valueTwo)
+                    self.open_list.append(valueTwo_number)
+                if 'high' in keyTwo:
+                    valueTwo_number = float(valueTwo)
+                    self.high_list.append(valueTwo_number)
+                if 'low' in keyTwo:
+                    valueTwo_number = float(valueTwo)
+                    self.low_list.append(valueTwo_number)
+                if 'close' in keyTwo:
+                    valueTwo_number = float(valueTwo)
+                    self.close_list.append(valueTwo_number)            
