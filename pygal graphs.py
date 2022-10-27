7b8fad0b50d35e879f52316e59b0b42f19bf4ef1
Naruto4soul <77596530+Naruto4soul@users.noreply.github.com>
2022-10-26 21:57:40 -0500

    Add files via upload

---
+++ pygal graphs.py
@@ -0,0 +1,14 @@
+def chart(self):
+    print("User Data chart type: ", self.user_data.chart_type)
+    if self.user_data.chart_type == 1:
+        chart = pygal.Bar(x_label_rotation=45, title=f"Stock data for {self.user_data.stock_symbol} stocks for {self.user_data.start_date} to {self.user_data.end_date})"
+    else:
+        chart = pygal.Line(x_label_rotation=45)
+    self.date_list.reverse()
+    chart.x_labels = self.date_list
+    chart.add("Open", self.open_list)
+    chart.add("Close", self.close_list)
+    chart.add("Low", self.low_list)
+    chart.add("High", self.hugh_list)
+    chart.render_in_browser()
+    
