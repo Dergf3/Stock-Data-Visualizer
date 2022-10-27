863f0ee031952156dee2413cd20067619b3bb139
jamesshawhan <70238458+jamesshawhan@users.noreply.github.com>
2022-10-27 10:13:57 -0500

    pulled title out of if else statement

--- pygal graphs.py
+++ pygal graphs.py
@@ -1,9 +1,10 @@
 def chart(self):
     print("User Data chart type: ", self.user_data.chart_type)
     if self.user_data.chart_type == 1:
-        chart = pygal.Bar(x_label_rotation=45, title=f"Stock data for {self.user_data.stock_symbol} stocks for {self.user_data.start_date} to {self.user_data.end_date})"
+        chart = pygal.Bar(x_label_rotation=45)
     else:
         chart = pygal.Line(x_label_rotation=45)
+    chart.title =(title=f"Stock data for {self.user_data.stock_symbol} stocks for {self.user_data.start_date} to {self.user_data.end_date})
     self.date_list.reverse()
     chart.x_labels = self.date_list
     chart.add("Open", self.open_list)
